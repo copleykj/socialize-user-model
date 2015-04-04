42588513aef42e118321d3693d59cf363147147d
Kelly <copleykj@gmail.com>
2015-04-03 21:03:17 -0400

    depend on new base-model

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: "socialize:user-model",
     summary: "A social user package",
-    version: "0.1.0",
+    version: "0.1.1",
     git: "https://github.com/copleykj/socialize-user-model.git"
 });
 
@@ -9,7 +9,7 @@ Package.onUse(function(api) {
     api.versionsFrom("1.0.2.1");
 
     api.use([
-        "meteor", "mongo", "underscore", "socialize:base-model@0.1.0", "copleykj:simple-publish@1.0.4",
+        "meteor", "mongo", "underscore", "socialize:base-model@0.1.2", "copleykj:simple-publish@1.0.4",
         "accounts-base", "aldeed:simple-schema@1.3.0", "meteorhacks:unblock@1.1.0", 
         "aldeed:collection2@2.3.2", "matb33:collection-hooks@0.7.9"
     ]);
