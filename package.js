746cedc7acc164eaf99d8c7990b4ce66aa1a1169
Kelly <copleykj@gmail.com>
2015-05-28 08:42:50 -0400

    remove deps that are now added and implied by base-model

--- package.js
+++ package.js
@@ -9,10 +9,11 @@ Package.onUse(function(api) {
     api.versionsFrom("1.0.2.1");
 
     api.use([
-        "meteor", "mongo", "underscore", "socialize:base-model@0.1.3",
-        "accounts-base", "aldeed:simple-schema@1.3.2", "meteorhacks:unblock@1.1.0", 
-        "aldeed:collection2@2.3.3", "matb33:collection-hooks@0.7.13"
+        "socialize:base-model@0.2.0",
+        "accounts-base"
     ]);
+
+    api.imply(["socialize:base-model", "accounts-base"]);
 
     //Add the user-model files
     api.addFiles("common/user-model.js");
