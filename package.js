8e0fbc9c38a0bfee808553a9eeedba5e540a65fc
Kelly <copleykj@gmail.com>
2015-06-30 12:35:40 -0400

    bump version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: "socialize:user-model",
     summary: "A social user package",
-    version: "0.1.3",
+    version: "0.1.4",
     git: "https://github.com/copleykj/socialize-user-model.git"
 });
 
