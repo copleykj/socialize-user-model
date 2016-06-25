a120f1716c3d58b5f09c54d0583ba94075539206
copleykj <copleykj@gmail.com>
2016-06-24 22:51:21 -0400

    update package.js

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
     name: "socialize:user-model",
     summary: "A social user package",
-    version: "0.1.7",
+    version: "1.0.0",
     git: "https://github.com/copleykj/socialize-user-model.git"
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom("1.0.2.1");
+    api.versionsFrom("1.3");
 
     api.use([
         "socialize:linkable-model@1.0.0",
