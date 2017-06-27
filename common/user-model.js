90c3d825437de0e0e7f7ed0c64b56f6bf264605a
Kelly Copley <copleykj@gmail.com>
2017-06-26 23:28:14 -0400

    users schema
    users collection requires a schema in cases where the user model is modified to be likeable, commentable, or postable. This way the schemas from these can also be attached.

--- common/user-model.js
+++ common/user-model.js
@@ -45,4 +45,46 @@ export class User extends LinkParent { //eslint-disable-line
 
 User.attachCollection(Meteor.users);
 
+const UsersSchema = new SimpleSchema({
+    username: {
+        type: String,
+        // For accounts-password, either emails or username is required, but not both. It is OK to make this
+        // optional here because the accounts-password package does its own validation.
+        // Third-party login packages may not require either. Adjust this schema as necessary for your usage.
+        optional: true,
+    },
+    emails: {
+        type: Array,
+        // For accounts-password, either emails or username is required, but not both. It is OK to make this
+        // optional here because the accounts-password package does its own validation.
+        // Third-party login packages may not require either. Adjust this schema as necessary for your usage.
+        optional: true,
+    },
+    'emails.$': {
+        type: Object,
+    },
+    'emails.$.address': {
+        type: String,
+        regEx: SimpleSchema.RegEx.Email,
+    },
+    'emails.$.verified': {
+        type: Boolean,
+    },
+    createdAt: {
+        type: Date,
+    },
+    // Make sure this services field is in your schema if you're using any of the accounts packages
+    services: {
+        type: Object,
+        optional: true,
+        blackbox: true,
+    },
+    heartbeat: {
+        type: Date,
+        optional: true,
+    },
+});
+
+User.attachSchema(UsersSchema);
+
 LinkableModel.registerParentModel(User);
