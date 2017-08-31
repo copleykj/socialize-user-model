6dee53cca937bde3dab9dc2e90f6c2cafb257666
Kelly Copley <copleykj@gmail.com>
2017-08-30 23:16:01 -0400

    remove unecessary docr info

--- common/user-model.js
+++ common/user-model.js
@@ -14,7 +14,6 @@ export class User extends LinkParent { //eslint-disable-line
     /**
     * The personal name of the user account, You if the the user represents the
     * currently logged in user, or this.username otherwise
-    * @method name
     * @returns {String} A name representation of the user account
     */
     displayName() {
@@ -23,7 +22,6 @@ export class User extends LinkParent { //eslint-disable-line
 
     /**
     * Check if the this user is the current logged in user or the specified user
-    * @method isSelf
     * @param   {Object}  user The user to check against
     * @returns {Boolean} Whether or not this user is the same as the specified user
     */
@@ -35,13 +33,11 @@ export class User extends LinkParent { //eslint-disable-line
 
     /**
     * Get the default email address for the user
-    * @method defaultEmail
     * @returns {String} The users default email address
     */
     defaultEmail() {
         return this.emails && this.emails[0].address;
     }
-
 }
 
 User.attachCollection(Meteor.users);
